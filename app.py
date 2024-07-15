e6add43a1dec5c12d810d331597721dd28317dcc
Fikry C Farenza <vickyfarenza@gmail.com>
2024-07-15 10:02:12 +0700

    :pencil:

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 if not hasattr(st, 'already_started_server'):
-    # Hack the fact that Python modules (like st) only load once to
-    # keep track of whether this file already ran.
-    st.already_started_server = True
+  # Hack the fact that Python modules (like st) only load once to
+  # keep track of whether this file already ran.
+  st.already_started_server = True
 
-    st.write('''
+  st.write('''
         The first time this script executes it will run forever because it's
         running a Flask server.
 
@@ -13,19 +13,23 @@ if not hasattr(st, 'already_started_server'):
         app.
     ''')
 
-    from flask import Flask
+  from flask import Flask
 
-    app = Flask(__name__)
+  app = Flask(__name__)
 
-    @app.route('/foo')
-    def serve_foo():
-        return 'This page is served via Flask!'
+  # @app.route('/foo')
+  # def serve_foo():
+  #   return 'This page is served via Flask!'
 
-    app.run(port=8888)
-
+  app.run(port=8888)
 
 # We'll never reach this part of the code the first time this file executes!
 
 # Your normal Streamlit app goes here:
 x = st.slider('Pick a number')
 st.write('You picked:', x)
+
+
+@app.route('/foo')
+def serve_foo():
+  return 'This page is served via Flask!'
